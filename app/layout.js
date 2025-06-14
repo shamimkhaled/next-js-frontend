6ec4233c6950ba478501b4fecf0c55e584793a1f
rohan181 <ab.rohan462@gmail.com>
2025-06-14 12:48:08 +0800

    layout

--- app/layout.js
+++ app/layout.js
@@ -29,7 +29,7 @@ export default function RootLayout({ children }) {
       >
         <CartProvider>
           <Navbar />
-          <main className="min-h-screen pt-16"> {/* Added pt-16 for navbar spacing */}
+          <main className="min-h-screen"> {/* Removed pt-16 to eliminate gray gap */}
             {children}
           </main>
           <Footer />
