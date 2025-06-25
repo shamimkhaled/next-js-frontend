f03a12835d8ff3b4824cb36e0079ce26b18c7109
rohan181 <ab.rohan462@gmail.com>
2025-06-25 13:42:01 +0800

    ui change

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -339,7 +339,7 @@ const ProductCard = ({ product }) => {
                 <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
                 </svg>
-                <span>Add</span>
+                <span>Add to Cart</span>
               </>
             )}
           </button>
